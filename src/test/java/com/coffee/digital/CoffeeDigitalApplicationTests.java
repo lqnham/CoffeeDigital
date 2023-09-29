e3aef5315f0ec081a6dbefa203baaab349280c46
lqnham <lqnham@gmail.com>
2023-09-29 15:10:27 +0700

    Initial commit

---
+++ src/test/java/com/coffee/digital/CoffeeDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffee.digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeDigitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
